69a4c641f73de812571cc984b746f85939b64b3f
fudosman <fudosman@gmail.com>
2023-11-09 18:10:12 +0100

    connecting the brochure to the google drive

--- js/index.js
+++ js/index.js
@@ -1,9 +1,12 @@
 document.getElementById('downloadBtn').addEventListener('click', function () {
+  // Replace 'FILE_ID' with the actual ID of your Google Drive file
+  var fileID = '1LW98cWDMHlh7RHGNJWRPwFycXj7IghQD';
+
   // Create a virtual anchor element
   var link = document.createElement('a');
 
-  // Set the href attribute to the file path
-  link.href = '../media/SK Forever.pdf';
+  // Set the href attribute to the direct download link
+  link.href = 'https://drive.google.com/uc?id=' + fileID;
 
   // Set the download attribute to specify the filename
   link.download = 'SK_Forever_Wedding_Program.pdf';
